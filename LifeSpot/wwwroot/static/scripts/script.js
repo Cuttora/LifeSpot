1455d20301f2dfae556b850700bf06de590ce738
Cuttora <leogon4arenko@yandex.ru>
2026-08-08 20:25:59 +0700

    29.7. Промежуточная сдача проекта (HW-03)

---
+++ LifeSpot/wwwroot/static/scripts/script.js
@@ -0,0 +1,51 @@
+﻿let session = new Map();
+function handleSession() {
+    
+    session.set("startDate", new Date().toLocaleString())
+    
+    session.set("userAgent", window.navigator.userAgent)
+}
+function checkAge()
+{
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+const inputParseFunction = function () {
+    return document.getElementsByTagName('input')[0].value.toLowerCase();
+}
+
+
+function filterContent() {
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
